5a89f9b343399f0f656e2e74a1ef086584ed19ea
Diego Confessor <diegondonascimento@gmail.com>
2024-09-20 08:37:20 -0300

    projeto-loja

---
+++ projetoloja/app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function TabLayout(){
+    return(
+        <Tabs>
+            <Tabs.Screen 
+                name="home"
+                options={{
+                        title: 'Início',
+                        tabBarIcon :({color}) => <FontAwesome size={28} name="home" color={color}/>
+                }}
+            />
+            <Tabs.Screen 
+                name="categories"
+                options={{
+                        title: 'Categorias',
+                        headerShown: false,
+                        tabBarIcon :({color}) => <FontAwesome size={28} name="archive" color={color}/>
+                }}
+            />
+            <Tabs.Screen 
+            name="about"
+            options={{
+                    title: 'Sobre mim',
+                    tabBarIcon :({color}) => <FontAwesome size={28} name="user" color={color}/>
+            }}
+        />
+        </Tabs>
+    );
+}
